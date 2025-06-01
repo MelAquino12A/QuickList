9083e241c2927abc6c411961ffa05bfafe74b5e8
Melissa Aquino <melissa.aquino.andrade@gmail.com>
2025-06-01 15:51:44 -0300

    fim do projeto com tudo salvo pq eu esqueci de salvar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem(){
     const itemName = document.querySelector("#item").value
@@ -50,6 +50,8 @@ function showItemsList() {
         
             
     } )
+
+    localStorage.setItem("items", JSON.stringify(items))
 }
     
 
@@ -81,3 +83,14 @@ function removeItem(itemName) {
 function removeHideWarningClass(){
     document.querySelector(".warning").classList.add("hide-warning")
 }
+
+function verifyLocalStorageItems() {
+    const localStorageItems = localStorage.getItem("items")
+
+    if(localStorageItems){
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
